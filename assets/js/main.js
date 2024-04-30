8bc60064fea9ba6fbd7e897c16f83af22f22a79a
Developersemja <developer.semja@gmail.com>
2024-04-30 12:08:24 +0530

    contact , overflow, Seo

--- assets/js/main.js
+++ assets/js/main.js
@@ -306,17 +306,3 @@ document.addEventListener('click', e => {
         cursor.classList.remove("expand");
     }, 500);
 });
-
-function sendemail(){
-   Email.send({
-      Host : "smtp.elasticemail.com",
-      Username : "username",
-      Password : "password",
-      To : 'them@website.com',
-      From : "you@isp.com",
-      Subject : "This is the subject",
-      Body : "And this is the body"
-  }).then(
-    message => alert(message)
-  );
-}
