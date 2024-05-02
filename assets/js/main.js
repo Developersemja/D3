51b107465a19e1657430eb328b85057004f130b3
Developersemja <developer.semja@gmail.com>
2024-05-02 17:40:28 +0530

    responsiveness mobile,small laptop

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,6 +69,7 @@ document.addEventListener("DOMContentLoaded", function() {
      });
    });
  
+ 
    // Function to scramble text
    function scrambleText(text) {
      return text.split('').sort(() => Math.random() - 0.5).join('');
